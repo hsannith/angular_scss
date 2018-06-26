5f789bc929c9bfe697e11bedf26d8fffee0f79b0
hitesh sannithi <hsannith@asu.edu>
2018-06-25 19:42:41 -0700

    single page application part 1

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,10 +1,12 @@
 import { Comment } from './comment';
 
 export class Dish {
+    id:number;
     name: string;
     image: string;
     category: string;
     label: string;
+    featured:boolean;
     price: string;
     description: string;
     comments: Comment[];
